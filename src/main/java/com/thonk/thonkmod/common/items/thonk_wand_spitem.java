8fbf5807f0e3ce1f7c6643073c53bb24cb32c964
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-05-08 15:35:41 -0500

    Added the Thonk Wand! It has the ability to summon lightning wherever the player looks, with a 5 second cooldown. Has a tooltip with lore and usage, accessible by holding down the shift and control keys.

---
+++ src/main/java/com/thonk/thonkmod/common/items/thonk_wand_spitem.java
@@ -0,0 +1,91 @@
+package com.thonk.thonkmod.common.items;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
+
+public class thonk_wand_spitem extends Item {
+
+    public thonk_wand_spitem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+        if (InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL)) {
+            tooltip.add(new StringTextComponent("\u00A77With a \u00A73right click\u00A77, you can summon\n" +
+                    " \u00A7elightning \u00A77wherever you look. However, this power\n comes with a \u00A74cooldown" +
+                    "\u00A77 of\u00A7c 5 seconds."));
+        } else if (InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT)) {
+            tooltip.add(new StringTextComponent("\u00A77This \u00A76legendary \u00A77item is rumored to posses" +
+                    "a power that allows the user to control \u00A7elightning \u00A77wherever they wish. However, " +
+                    "the owner must see their target for the wand to do its work."));
+        }
+        else {
+            // "Hold " + "\u00A7e" + "Shift" + "\u00A77" + " for More Information"
+            tooltip.add(new TranslationTextComponent("tooltip.thonk_wand_spitem.hold_shift"));
+            tooltip.add(new TranslationTextComponent("tooltip.thonk_wand_spitem.hold_control"));
+        }
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand handIn) {
+        // The endpoint of the ray (where it travels to)
+        double rayLength = 100.0;
+        Vector3d playerRotation = player.getViewVector(0);
+        Vector3d rayPath = playerRotation.scale(rayLength);
+        // The start and end points of the ray
+        Vector3d from = player.getEyePosition(0);
+        Vector3d to = from.add(rayPath);
+        // Create a ray, then cast it.
+        RayTraceContext rayCtx = new RayTraceContext(from, to, RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.ANY, null);
+        BlockRayTraceResult rayHit = world.clip(rayCtx);
+
+        // Now use the results
+        if (rayHit.getType() != RayTraceResult.Type.MISS){
+            // If we get a hit (If the ray does not miss)
+            Vector3d hitLocation = rayHit.getLocation();
+
+            LightningBoltEntity lightning = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
+            lightning.setPos(hitLocation.x, hitLocation.y, hitLocation.z);
+            world.addFreshEntity(lightning);
+
+            // The cooldown is a 1/10 of the specified integer, not ticks. (So 50 would set it to 5 seconds)
+            player.getCooldowns().addCooldown(this, 50);
+
+        }
+
+        return super.use(world, player, handIn);
+    }
+
+
+    /*
+            // NOTE: The following gives the player a potion effect, followed by the duration and multiplier integers.
+             It is important to note that the duration and the effect are not measured as you may think. The duration is
+             measured with in-game ticks. There are 20 in-game ticks for every 1 second, so a duration listed as 60
+             would last for 3 seconds. The multiplier will be one more than whatever integer you supply a multiplier of
+             5, it would be 6 in-game. Setting the multiplier as 0 or not specifying it at all will therefore set the
+             multiplier as 1, which is the default of a potion effect. //
+            playerIn.addEffect(new EffectInstance(Effects.MOVEMENT_SPEED, 60)); // Duration then multiplier
+     */
+
+}
--- src/main/java/com/thonk/thonkmod/core/init/ItemInit.java
+++ src/main/java/com/thonk/thonkmod/core/init/ItemInit.java
@@ -1,5 +1,6 @@
 package com.thonk.thonkmod.core.init;
 
+import com.thonk.thonkmod.common.items.thonk_wand_spitem;
 import com.thonk.thonkmod.thonkmod;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -12,10 +13,15 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, thonkmod.MOD_ID);
 
     // ITEMS
-    public static final RegistryObject<Item> THONK_ITEM = ITEMS.register("thonk_item", () -> new Item(new Item.Properties().tab(thonkmod.THONKMOD_TAB)));
+    public static final RegistryObject<Item> THONK_ITEM = ITEMS.register("thonk_item",
+            () -> new Item(new Item.Properties().tab(thonkmod.THONKMOD_TAB)));
+
+    // SPECIAL ITEMS
+    public static final RegistryObject<thonk_wand_spitem> THONK_WAND_SPITEM = ITEMS.register("thonk_wand_spitem",
+            () -> new thonk_wand_spitem(new Item.Properties().tab(thonkmod.THONKMOD_TAB)));
 
     // BLOCK ITEMS
-    public static final RegistryObject<BlockItem> THONK_ORE_BLOCK = ITEMS.register("thonk_ore_block", () -> new BlockItem(BlockInit.THONK_ORE_BLOCK.get(),
-            new Item.Properties().tab(thonkmod.THONKMOD_TAB)));
+    public static final RegistryObject<BlockItem> THONK_ORE_BLOCK = ITEMS.register("thonk_ore_block",
+            () -> new BlockItem(BlockInit.THONK_ORE_BLOCK.get(), new Item.Properties().tab(thonkmod.THONKMOD_TAB)));
 
 }
